a14b32dc9ee018f8d49c525b3f5a2b9da65b0a85
LaiFh <1245842972@qq.com>
2017-12-22 22:57:22 +0800

    TextFileTest.java修改

--- src/TextFileTest.java
+++ src/TextFileTest.java
@@ -1,3 +1,5 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Calendar;
 import java.util.Date;
@@ -6,37 +8,57 @@ import java.util.Scanner;
 
 public class TextFileTest {
     public static void main(String[] args) {
+        //System.out.println(System.getProperty("user.dir"));
+        try {
+            Employee employee1 = new Employee("jack", 5000, 1991, 6, 8);
+            Employee employee2 = new Employee("ming", 8000, 1992, 9, 10);
+            Employee employee3 = new Employee("mary", 3000, 1993, 10, 27);
+            Employee[] employees = {employee1, employee2, employee3};
+            TextFileTest test=new TextFileTest();
 
+            try (PrintWriter out = new PrintWriter("employee.txt")) {
+                test.writeData(employees,out);
+            }
+
+            try(Scanner in=new Scanner(new FileInputStream("employee.txt"))){
+                Employee[] employees1=test.readData(in);
+                for(Employee employee:employees){
+                    System.out.println(employee);
+                }
+            }
+        } catch (FileNotFoundException fe) {
+            fe.printStackTrace();
+        }
     }
 
     private Employee[] readData(Scanner in) {
         int length;
         length = in.nextInt();
-        Employee[] employees=new Employee[length];
+        Employee[] employees = new Employee[length];
         in.nextLine();
         for (int i = 0; i < length; i++) {
-            employees[i]=readEmployee(in);
+            employees[i] = readEmployee(in);
         }
         return employees;
     }
 
     private void writeData(Employee[] employees, PrintWriter out) {
         out.println(employees.length);
-        for (int i=0;i<employees.length;i++){
-            writeEmployee(employees[i],out);
+        for (int i = 0; i < employees.length; i++) {
+            writeEmployee(employees[i], out);
         }
     }
 
     private Employee readEmployee(Scanner in) {
         String empStr = in.nextLine();
-        String[] employStrs = empStr.split("|");
+        String[] employStrs = empStr.split("\\|");
         Employee employee = new Employee();
         employee.setName(employStrs[0]);
         employee.setSalary(Double.parseDouble(employStrs[1]));
-        GregorianCalendar calendar=new GregorianCalendar(Integer.parseInt(employStrs[2]), Integer.parseInt(employStrs[3]),
+        GregorianCalendar calendar = new GregorianCalendar(Integer.parseInt(employStrs[2]), Integer.parseInt(employStrs[3]),
                 Integer.parseInt(employStrs[4]));
         employee.setBirthDate(calendar.getTime());
-        return null;
+        return employee;
     }
 
     private void writeEmployee(Employee employee, PrintWriter out) {
@@ -55,10 +77,12 @@ class Employee {
     public Employee() {
     }
 
-    public Employee(String name, double salary, Date birthDate) {
-        this.birthDate = birthDate;
+    public Employee(String name, double salary, int year, int month, int date) {
         this.name = name;
         this.salary = salary;
+        GregorianCalendar calendar = new GregorianCalendar(year, month, date);
+        Date date1=new Date(calendar.getTimeInMillis());
+        birthDate=date1;
     }
 
     public String getName() {
@@ -83,5 +107,10 @@ class Employee {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return name+","+salary+","+birthDate.toString();
     }
 }
